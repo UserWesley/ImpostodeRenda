a7ef8b3da5505231d797663e4b5b9592f8a1b23e
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-13 09:47:00 -0300

    Primeiro Commit - Estrutura Inicial

---
+++ ImpostodeRenda/index.php
@@ -0,0 +1,34 @@
+<?php
+?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	
+	<meta charset = "utf-8">
+	
+	<!-- Visualização Mobile -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Incluindo Bootstrap CSS -->
+	<link href="_bootstrap-3.3.6-dist/_css/bootstrap.min.css" rel="stylesheet" media="screen">
+	
+	<!-- Incluindo Bootstrap JavaScript-->
+	<script src="_bootstrap-3.3.6-dist/_js/bootstrap.min.js"></script>
+	
+</head>
+
+<body>
+	
+	<header> Cálculo de Imposto de Renda</header>
+	<form class="form-horizontal">
+		
+		<label for= "idSalarioBruto">Salário Bruto :</label> 
+		<input type = "text" name = "numberSalarioBruto" id = "idSalarioBruto" placeholder = "2.000,00"min = "0" required>
+
+	</form>
+
+</body>
+
+</html>
